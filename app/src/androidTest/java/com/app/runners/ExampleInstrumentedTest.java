ebf44e03f33dcc934584c85e235f955bdd3bb56a
nmundru <nmundru@axxess.com>
2019-09-21 13:02:50 -0500

    first commit

--- app/src/androidTest/java/com/app/runners/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/app/runners/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
 package com.app.runners;
 
 import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
+
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
