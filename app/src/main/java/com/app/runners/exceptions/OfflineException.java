01c36e6e77004b3b3b88adbc93f9c8a7a3f5dcb2
nmundru <nmundru@axxess.com>
2019-09-25 13:24:56 -0500

    fixed app for pie

--- app/src/main/java/com/app/runners/exceptions/OfflineException.java
+++ app/src/main/java/com/app/runners/exceptions/OfflineException.java
@@ -3,8 +3,4 @@ package com.app.runners.exceptions;
 public class OfflineException extends Throwable {
 
     public OfflineException() {}
-    
-    public OfflineException(String message) {
-        super(message);
-    }
 }
